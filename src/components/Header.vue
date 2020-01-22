4df3d12b941beda6a4a89d8c33f403f9c373bace
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2020-01-22 16:43:52 -0500

    created header and footer

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="row">
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <a class="navbar-brand" href="/">Much To-Do About Nothing</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
