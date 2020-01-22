0bb131b0d772d6bbd8979a74afccd41b0ae0cbd9
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2020-01-22 17:14:36 -0500

    minor styling of home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,30 @@
                 <h2>Your personal organization app</h2>
                 <h5>username:</h5>
                 <h5>password:</h5>
+                <button class="btn btn-success">Sign In</button>
                 <button class="btn btn-success">Sign Up</button>
             </div>
         </div>
     </div>
 </template>
+
+<style>
+    #home {
+        background: url("../../src/assets/background.jpg");
+        background-size: cover;
+        height: 800px;
+        padding: 10%;
+    }
+
+    h1, h2 {
+        margin: 6%;
+    }
+
+    .background {
+        background: rgb(122, 122, 122);
+        opacity: 0.8;
+        max-width: 60%;
+        margin: 0 auto;
+        padding: 20px 0;
+    }
+</style>
